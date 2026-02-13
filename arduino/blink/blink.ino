5d9a287eb12c1f90d3e662877eca717bd5ce6b14
microsoftalternate <microsoftalternate@gmail.com>
2026-02-13 04:03:06 +0000

    Add Arduino blink sketch and ignore build artifacts

---
+++ arduino/blink/blink.ino
@@ -0,0 +1,12 @@
+// Arduino Uno Blink test (built-in LED on pin 13)
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
